4e2918333b455a9c44d7afcd842612c97b110fbe
Tussis88 <andreab88@hotmail.it>
2024-01-23 01:32:43 +0100

    neotree ora mostra i file nascosti di base

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,19 +1,23 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-        require('neo-tree').setup({
-            close_if_last_window = true,
-        })
-    end
+	-- file explorer laterale
+	-- https://github.com/nvim-treesitter/nvim-treesitter
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				-- questa impostazione rende sempre visibili i file nascosti, altrimenti bisognere usare il tasto H
+				filtered_items = {
+					visible = true,
+				},
+			},
+			close_if_last_window = true,
+		})
+	end,
 }
-
-
--- file explorer laterale 
--- https://github.com/nvim-treesitter/nvim-treesitter
