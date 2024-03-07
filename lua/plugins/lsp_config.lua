4ad1b2319e690e471ca01a26add6f224677ed4fa
Tussis88 <andreab88@hotmail.it>
2024-03-07 22:30:40 +0100

    Fixato none-ls e ripristinato flake8
    Aggiunta una dependency a none-ls, in questo modo riesce a gestire i
    pacchetti non più supportati
    
    eslint_d funziona correttamente, ma bisogna configurarlo per poterlo
    utilizzare. Al momento non ne vedo neceessità

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,32 +1,41 @@
 return {
-    {
-        -- Mason installa i server lsp
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        -- fa da ponte tra mason e lspconfig. qui si inseriscono i linguaggi che voglio installare di base
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "html", "cssls", "tsserver", "marksman" },
-            })
-        end,
-    },
-    {
-        -- gestisce l'lsp vero e proprio. in remap.lua ho inserito tutti i keybinds
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.pyright.setup({ capabilities = capabilities })
-            lspconfig.html.setup({ capabilities = capabilities })
-            lspconfig.cssls.setup({ capabilities = capabilities })
-            lspconfig.tsserver.setup({ capabilities = capabilities })
-            lspconfig.marksman.setup({ capabilities = capabilities })
-        end,
-    },
+	{
+		-- Mason installa i server lsp
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- fa da ponte tra mason e lspconfig. qui si inseriscono i linguaggi che voglio installare di base
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "html", "cssls", "tsserver", "marksman" },
+			})
+		end,
+	},
+	{
+		-- gestisce l'lsp vero e proprio. in remap.lua ho inserito tutti i keybinds
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysys = {
+							autoSearchPaths = true,
+						},
+					},
+				},
+			})
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.marksman.setup({ capabilities = capabilities })
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,27 @@
 return {
-    -- formatter. https://github.com/jose-elias-alvarez/null-ls.nvim
-    -- Bisogna installare i formatters con Mason e poi inserirli manualmente qui.
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                -- lua || formatter
-                null_ls.builtins.formatting.stylua,
-                -- python || formatter
-                null_ls.builtins.formatting.black,
-                --python || linter
-                null_ls.builtins.diagnostics.pylint.with({ extra_args = { "--max-line-length", "120" } }),
-                -- javascript, css, markdown, html || formatter
-                null_ls.builtins.formatting.prettier,
-                -- javascript || linter
-                -- null_ls.builtins.diagnostics.eslint,
-                -- markdown || linter
-                null_ls.builtins.diagnostics.alex,
-            },
-        })
-    end,
+	-- formatter. https://github.com/jose-elias-alvarez/null-ls.nvim
+	-- Bisogna installare i formatters con Mason e poi inserirli manualmente qui.
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- lua || formatter
+				null_ls.builtins.formatting.stylua,
+				-- python || formatter
+				null_ls.builtins.formatting.black,
+				--python || linter
+				require("none-ls.diagnostics.flake8").with({ extra_args = { "--max-line-length", "120" } }),
+				-- javascript, css, markdown, html || formatter
+				null_ls.builtins.formatting.prettier,
+				-- javascript || linter
+				-- require("none-ls.diagnostics.eslint_d"),     per far funzionare correttamente eslint devo creare un file di configurazione (o generico o per il singolo progetto)
+				-- markdown || linter
+				null_ls.builtins.diagnostics.alex,
+			},
+		})
+	end,
 }
