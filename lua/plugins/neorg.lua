d06f401a670d9647f3f391754129a692fee94485
Tussis88 <andreab88@hotmail.it>
2024-03-19 00:24:52 +0100

    Aggiunti keybinds a Neorg
    Aggiunto keybind per la tabella dei contenuti, aggiunto colore a
    grosseto e italico.
    
    Spostato tutti i plugin relativi all'UI dentro ui.lua

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,49 +1,54 @@
 return {
-	-- Orgmode per neovim
-	-- https://github.com/nvim-neorg/neorg?tab=readme-ov-file
-	{
-		"nvim-neorg/neorg",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		build = ":Neorg sync-parsers",
-		lazy = true,
-		ft = "norg",
-		cmd = "Neorg",
-		-- tag = "*",
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {}, -- Loads default behaviour
-					["core.highlights"] = {
-						config = {
-							highlights = {
-								headings = {
-									["1"] = { title = "+markdownH1", prefix = "+markdownH1Delimiter" },
-									["2"] = { title = "+markdownH2", prefix = "+markdownH2Delimiter" },
-									["3"] = { title = "+markdownH3", prefix = "+markdownH3Delimiter" },
-									["4"] = { title = "+markdownH4", prefix = "+markdownH4Delimiter" },
-									["5"] = { title = "+markdownH5", prefix = "+markdownH5Delimiter" },
-									["6"] = { title = "+markdownH6", prefix = "+markdownH6Delimiter" },
-									-- riferimenti a rose-pine https://github.com/rose-pine/neovim/blob/c52167563e6aa44b5fa6fe007faa2bcac71050f0/lua/rose-pine.lua#L570C3-L581C48
-								},
-							},
-						},
-					},
-					["core.concealer"] = {
-						config = {
-							icon_preset = "diamond",
-							icons = { heading = { icons = { "❖", "◆", "◈", "◇", "⟡", "⋄" } } },
-						},
-					},
-					["core.dirman"] = { -- Manages Neorg workspaces
-						config = {
-							workspaces = {
-								notes = "~/documents/notes",
-							},
+    -- Orgmode per neovim
+    -- https://github.com/nvim-neorg/neorg?tab=readme-ov-file
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        build = ":Neorg sync-parsers",
+        lazy = true,
+        ft = "norg",
+        cmd = "Neorg",
+        -- tag = "*",
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.highlights"] = {
+                        config = {
+                            highlights = {
+                                headings = {
+                                    ["1"] = { title = "+markdownH1", prefix = "+markdownH1Delimiter" },
+                                    ["2"] = { title = "+markdownH2", prefix = "+markdownH2Delimiter" },
+                                    ["3"] = { title = "+markdownH3", prefix = "+markdownH3Delimiter" },
+                                    ["4"] = { title = "+markdownH4", prefix = "+markdownH4Delimiter" },
+                                    ["5"] = { title = "+markdownH5", prefix = "+markdownH5Delimiter" },
+                                    ["6"] = { title = "+markdownH6", prefix = "+markdownH6Delimiter" },
+                                    -- riferimenti a rose-pine https://github.com/rose-pine/neovim/blob/c52167563e6aa44b5fa6fe007faa2bcac71050f0/lua/rose-pine.lua#L570C3-L581C48
+                                },
+                            },
+                        },
+                    },
+                    ["core.concealer"] = {
+                        config = {
+                            icon_preset = "diamond",
+                            -- riordina i diamani dal più grande al più piccolo
+                            icons = { heading = { icons = { "❖", "◆", "◈", "◇", "⟡", "⋄" } } },
+                        },
+                    },
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/documents/notes",
+                            },
                             default_workspace = "notes",
-						},
-					},
-				},
-			})
-		end,
-	},
+                        },
+                    },
+                    -- chiude l`indice degli header una volta selezionato uno
+                    ["core.qol.toc"] = {
+                        config = { close_after_use = true },
+                    },
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,77 +1,123 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				variant = "moon",
-				disable_background = false,
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon",
+                disable_background = false,
                 styles = {
                     transparency = true,
-                }
-			})
+                },
+            })
             vim.cmd([[colorscheme rose-pine]])
-		end,
-	},
-	{
-		-- lualine      https://github.com/nvim-lualine/lualine.nvim
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "rose-pine-alt", --"rose-pine-alt" per la versione con colori invertiti
-					disabled_filetypes = { "alpha", "dashboard", "neo-tree" },
-					-- component_separators = { left = '', right = '' },
-					component_separators = { left = "|", right = "|" },
-					-- section_separators = { left = '', right = '' },
-					section_separators = { left = "", right = "" },
-				},
-				sections = {
-					lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
-				},
-			})
-		end,
-	},
+            -- colori per italico e grassetto in neorg e markdown
+            vim.api.nvim_set_hl(0, "@markup.italic", { italic = true, fg = "#ea9d34" })
+            vim.api.nvim_set_hl(0, "@markup.strong", { bold = true, fg = "#d7827e" })
+        end,
+    },
+    {
+        -- lualine      https://github.com/nvim-lualine/lualine.nvim
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "rose-pine-alt", --"rose-pine-alt" per la versione con colori invertiti
+                    disabled_filetypes = { "alpha", "dashboard", "neo-tree" },
+                    -- component_separators = { left = '', right = '' },
+                    component_separators = { left = "|", right = "|" },
+                    -- section_separators = { left = '', right = '' },
+                    section_separators = { left = "", right = "" },
+                },
+                sections = {
+                    lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
+                },
+            })
+        end,
+    },
+    {
+        -- https://github.com/yamatsum/nvim-cursorline
+        -- sottolinea le parole uguali a quella sopra cui si trova il cursore
+        "yamatsum/nvim-cursorline",
+        config = function()
+            require("nvim-cursorline").setup({
+                cursorline = {
+                    enable = false,
+                },
+                cursorword = {
+                    enable = true,
+                    min_lenght = 3,
+                    hl = { underline = true },
+                },
+            })
+        end,
+    },
+    {
+        -- colora la indent-line in base alla funzione parente
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        config = function()
+            local scope = "focus"
+            -- local indent = "passive"
+            local hooks = require("ibl.hooks")
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "focus", { fg = "#8f79c1" })
+                -- vim.api.nvim_set_hl(0, "passive", { fg = "#41425e" })
+            end)
+            require("ibl").setup({
+                scope = {
+                    highlight = scope,
+                    show_start = false,
+                    show_end = false,
+                },
+                indent = {
+                    -- highlight = indent,
+                    smart_indent_cap = true,
+                },
+            })
+        end,
+    },
 }
 
-    -- SOLARIZED OSAKA
-	-- {
-	--     -- tema solarized osaka https://github.com/craftzdog/solarized-osaka.nvim
-	--     "craftzdog/solarized-osaka.nvim",
-	--     lazy = false,
-	--     priority = 1000,
-	--     config = function()
-	--         require("solarized-osaka").setup({
-	--             styles = {
-	--                 functions = { bold = true },
-	--                 floats = "transparent",
-	--                 sidebars = "transparent",
-	--             },
-	--             dim_inactive = true,
-	--         })
-	--         vim.cmd([[colorscheme solarized-osaka]])
-	--     end,
-	-- },
-    --
-    -- TOKIONIGHT
-    -- {
-    --     -- tema tokyonight https://github.com/folke/tokyonight.nvim
-    --     "folke/tokyonight.nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         require("tokyonight").setup({
-    --             style = "dusk",
-    --             transparent = true,
-    --             styles = {
-    --                 functions = { bold = true },
-    --                 floats = "transparent",
-    --                 sidebars = "transparent",
-    --             },
-    --         })
-    --         vim.cmd([[colorscheme tokyonight]])
-    --         vim.api.nvim_set_hl(0, "@markup.italic", { italic = true })
-    --     end,
-    -- },
+-- SOLARIZED OSAKA
+-- {
+--     -- tema solarized osaka https://github.com/craftzdog/solarized-osaka.nvim
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("solarized-osaka").setup({
+--             styles = {
+--                 functions = { bold = true },
+--                 floats = "transparent",
+--                 sidebars = "transparent",
+--             },
+--             dim_inactive = true,
+--         })
+--         vim.cmd([[colorscheme solarized-osaka]])
+--     end,
+-- },
+--
+-- TOKIONIGHT
+-- {
+--     -- tema tokyonight https://github.com/folke/tokyonight.nvim
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("tokyonight").setup({
+--             style = "dusk",
+--             transparent = true,
+--             styles = {
+--                 functions = { bold = true },
+--                 floats = "transparent",
+--                 sidebars = "transparent",
+--             },
+--         })
+--         vim.cmd([[colorscheme tokyonight]])
+--         vim.api.nvim_set_hl(0, "@markup.italic", { italic = true })
+--     end,
+-- },
