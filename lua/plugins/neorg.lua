b0b74f3f140488349b8157ad1e7faf8c4527993f
Tussis88 <andreab88@hotmail.it>
2024-03-25 00:29:01 +0100

    Aggiornato neorg alla versione 8
    Installato luarocks e modificato il file neorg.lua per risolvere le
    breaking changes dell'ultimo aggiornamento

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,11 +1,19 @@
 return {
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+    },
     -- Orgmode per neovim
     -- https://github.com/nvim-neorg/neorg?tab=readme-ov-file
     {
         "nvim-neorg/neorg",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        build = ":Neorg sync-parsers",
-        lazy = true,
+        dependencies = {
+            "luarocks.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        -- build = ":Neorg sync-parsers",
+        lazy = false,
         ft = "norg",
         cmd = "Neorg",
         -- tag = "*",
@@ -17,13 +25,13 @@ return {
                         config = {
                             highlights = {
                                 headings = {
-                                --     ["1"] = { title = "+markdownH1", prefix = "+markdownH1Delimiter" },
-                                --     ["2"] = { title = "+markdownH2", prefix = "+markdownH2Delimiter" },
-                                --     ["3"] = { title = "+markdownH3", prefix = "+markdownH3Delimiter" },
-                                --     ["4"] = { title = "+markdownH4", prefix = "+markdownH4Delimiter" },
-                                --     ["5"] = { title = "+markdownH5", prefix = "+markdownH5Delimiter" },
-                                --     ["6"] = { title = "+markdownH6", prefix = "+markdownH6Delimiter" },
-                                --     -- riferimenti a rose-pine https://github.com/rose-pine/neovim/blob/c52167563e6aa44b5fa6fe007faa2bcac71050f0/lua/rose-pine.lua#L570C3-L581C48
+                                    --     ["1"] = { title = "+markdownH1", prefix = "+markdownH1Delimiter" },
+                                    --     ["2"] = { title = "+markdownH2", prefix = "+markdownH2Delimiter" },
+                                    --     ["3"] = { title = "+markdownH3", prefix = "+markdownH3Delimiter" },
+                                    --     ["4"] = { title = "+markdownH4", prefix = "+markdownH4Delimiter" },
+                                    --     ["5"] = { title = "+markdownH5", prefix = "+markdownH5Delimiter" },
+                                    --     ["6"] = { title = "+markdownH6", prefix = "+markdownH6Delimiter" },
+                                    --     -- riferimenti a rose-pine https://github.com/rose-pine/neovim/blob/c52167563e6aa44b5fa6fe007faa2bcac71050f0/lua/rose-pine.lua#L570C3-L581C48
                                 },
                             },
                         },
